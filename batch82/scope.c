b1174662a080baa00cbcd1ee72ad224e3104a9cf
g31pranjal <g31.pranjal@gmail.com>
2017-04-20 19:01:04 +0530

    closure 1

--- batch82/scope.c
+++ batch82/scope.c
@@ -164,11 +164,14 @@ int printScopeStructure(symbolScope * head) {
 
 		// identifier - usage - type - line declared - depth - width - offset
 		
+		printf("IDENTIFIER \t\t USAGE \t\t TYPE \t\t LINE NO. \t\t SCOPE LEVEL \t\t WIDTH \t\t OFFSET\n");
+		printf("----------------------------------------------------------------------------------------------------------------\n\n");
+
 		if(seHead->isArray == 1) {
-			printf("%s \t\t %s \t\t Array(%d, %s) \t\t %d \t\t %d \t\t %d \t\t , OFFSET : %d\n", seHead->identifier, usg[seHead->usage], seHead->endInd - seHead->startInd, seHead->type, seHead->lineInit, depth, seHead->width, seHead->offset );
+			printf("%s \t\t %s \t\t Array(%d, %s) \t\t %d \t\t %d \t\t %d \t\t %d\n", seHead->identifier, usg[seHead->usage], seHead->endInd - seHead->startInd, seHead->type, seHead->lineInit, depth, seHead->width, seHead->offset );
 		}
 		else {
-			printf("%s \t\t %s \t\t %s \t\t %d \t\t %d \t\t %d \t\t, OFFSET : %d\n", seHead->identifier, usg[seHead->usage], seHead->type, seHead->lineInit, depth, seHead->width, seHead->offset );
+			printf("%s \t\t %s \t\t %s \t\t %d \t\t %d \t\t %d \t\t %d\n", seHead->identifier, usg[seHead->usage], seHead->type, seHead->lineInit, depth, seHead->width, seHead->offset );
 		}
 
 
